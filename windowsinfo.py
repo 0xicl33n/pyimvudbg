77de96c67a006583d048ad91178a522e1e0fa7e8
Chris <0xicl33n@gmail.com>
2016-12-20 12:28:56 -0800

    Delete old crap

--- windowsinfo.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-#!/c/Python27/python.exe
-import getpass
-user = str(getpass.getuser())
-client = '''C:\Users\\"%s"\\AppData\\Roaming\\IMVUClient\\imvuclient.exe'''%(user)
-def path():
-	return client
